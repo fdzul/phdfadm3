4665058e5c29549e8403a04d0c60c88df8a8be0b
fdzul <felipe.dzul.m@gmail.com>
2020-08-17 21:57:19 -0500

    this commit is for add the params

--- R/glmm_binary.R
+++ R/glmm_binary.R
@@ -1,15 +1,26 @@
+#' generalize linear mixed model for binary data
+#'
+#' @param x is the dataset for modeling.
+#' @param aproximation is the strategy for aproximation.
+#' @param integration is the strategy for integration.
+#' @param sinave_new is TRUE for dengue dataset of 2016 to 2020. FALSE for dengue dataset from 2008 to 2015.
+#'
+#' @return a inla object.
+#' @export
+#'
+#' @examples
 glmm_binary <- function(x, aproximation, integration, sinave_new){
-    
+
     if(sinave_new == TRUE){
         x$caso_control <- ifelse(x$ESTATUS_CASO == "POSITIVO", 1, 0)
     } else{
         x$caso_control <- ifelse(x$VEC_EST == "POSITIVO", 1, 0)
     }
-    
+
     #family1 = "binomial"
     #control.family1 = list(control.link=list(model= link))
-    
-    INLA::inla(formula = caso_control ~ eggs + 
+
+    INLA::inla(formula = caso_control ~ eggs +
                    f(index, model = "iid") + ## id of cases
                    f(manzana, model = "iid") +
                    f(sector, model = "iid") +
@@ -22,7 +33,7 @@ glmm_binary <- function(x, aproximation, integration, sinave_new){
                #control.family = list(control.link = list(model = link)),
                #control.family = control.family1,
                #control.link = list(model=link),
-               control.inla = list(strategy = aproximation, 
+               control.inla = list(strategy = aproximation,
                                    int.strategy = integration,
                                    correct = TRUE),
                control.predictor = list(compute = TRUE, link = 1),
